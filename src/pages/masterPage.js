8ccba8f378c2974c562513ca78d679d05ef4c761
TamaPRIMA2502 <ahtamar@gmail.com>
2026-09-16 10:44:06 +0200

    Add direct URL trigger for PRIMA quick booking lightbox

--- src/pages/masterPage.js
+++ src/pages/masterPage.js
@@ -1,10 +1,8 @@
-// API Reference: https://www.wix.com/velo/reference/api-overview/introduction
-// “Hello, World!” Example: https://learn-code.wix.com/en/article/hello-world
+import wixLocationFrontend from 'wix-location-frontend';
+import wixWindowFrontend from 'wix-window-frontend';
 
 $w.onReady(function () {
-    // Write your JavaScript here
-
-    // To select an element by ID use: $w('#elementID')
-
-    // Click 'Preview' to run your code
+    if (wixLocationFrontend.query.open === 'booking') {
+        wixWindowFrontend.openLightbox('Швидка форма запису');
+    }
 });
